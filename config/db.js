727c9837c94c6d73f9c152ffb41f64bd102eb9ca
Aakriti Gupta <draakritig@gmail.com>
2020-10-24 21:40:58 -0400

    Finally done

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 module.exports =
 {
-    "URI": "mongodb://localhost:27017/business_list"
+    //"URI": "mongodb://localhost:27017/business_list"
+    "URI": "mongodb+srv://aakriti:k1NWyIi9LWxza56T@databasemongo.60wdf.mongodb.net/business_list?retryWrites=true&w=majority"
 }
 module.exports.Secret = "SomeSecret";
